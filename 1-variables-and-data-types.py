6cb76c6f58d3e260ce6c46883f11fd905d8badfe
Cynox336 <Cynox336@gmail.com>
2026-09-22 08:34:53 +0200

    1st exercise completed

--- 1-variables-and-data-types.py
+++ 1-variables-and-data-types.py
@@ -10,6 +10,8 @@ Asígnale el valor "¡Hola, Mundo!".
 Imprime el valor de la variable en la consola.
 """
 # Escribe tu código aquí
+mensaje = "¡Hola, Mundo!"
+print(mensaje)
 
 """
 --- Ejercicio 2 Variables---
@@ -19,6 +21,8 @@ Imprime el valor de la variable en la consola.
 Escribe en un comentario de línea lo que sucede.
 """
 # Escribe tu código aquí
+mensaje = "Hello world!"
+print(mensaje)
 
 """
 --- Ejercicio 3 Tipos de datos---
@@ -27,4 +31,20 @@ bool, list, tuple, dicctionary and set.
 Imprime cada variable y el tipo de dato o colección que almacena en la consola.
 """
 # Escribe tu código aquí
+mi_string = "Soy un texto"
+mi_int = 42
+mi_float = 3.14
+mi_bool = True
+mi_lista = [1, 2, 3]
+mi_tupla = (4, 5, 6)
+mi_diccionario = {"clave": "valor"}
+mi_set = {7, 8, 9}
 
+print(mi_string, type(mi_string))
+print(mi_int, type(mi_int))
+print(mi_float, type(mi_float))
+print(mi_bool, type(mi_bool))
+print(mi_lista, type(mi_lista))
+print(mi_tupla, type(mi_tupla))
+print(mi_diccionario, type(mi_diccionario))
+print(mi_set, type(mi_set))
